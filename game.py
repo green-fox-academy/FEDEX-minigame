153aa5e19d8bc43965863a3a75b1ed264f650254
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-10 21:59:50 +0100

    feat(game): display fruits random

--- game.py
+++ game.py
@@ -1,12 +1,15 @@
 import os
 import pygame
 import random
+from fruits import Fruits
+
 
 pygame.init()
 
 BLACK = [0, 0, 0]
 WHITE = [255, 255, 255]
-SIZE = [600, 600]
+SIZE = [700, 700]
+
 
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("Mini Game")
@@ -14,10 +17,13 @@ background = pygame.image.load(os.path.join("assets", "screen_tree.jpg"))
 background = pygame.transform.scale(background, SIZE)
 
 clock = pygame.time.Clock()
-
+fruits = [pygame.image.load(os.path.join("assets", 'orange.png')), pygame.image.load(os.path.join("assets", 'banana.png')), pygame.image.load(os.path.join("assets", 'melon.png')), pygame.image.load(os.path.join("assets", 'apple.png'))]
+one_fruit = random.choice(fruits)
+for i in range(100):
+    x = random.randrange(0, 700)
+    y = random.randrange(0, 700)
 # pygame.mouse.set_visible(False)
 # pygame.event.set_grab(True)
-
 done = False
 while not done:
     for event in pygame.event.get():  # User did something
@@ -26,8 +32,8 @@ while not done:
 
     screen.fill(WHITE)
     screen.blit(background, (0,0))
+    screen.blit(one_fruit, (20,20))
     pygame.display.update()
-
 
 pygame.quit()
 quit()
