cbb0c582f96aa673350742f88e9536900e660d55
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-10 16:59:04 +0100

    feat(game): add background image

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import os
 import pygame
 import random
 
@@ -9,22 +10,24 @@ SIZE = [600, 600]
 
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption("Mini Game")
-
-snow_list = []
-for i in range(50):
-    x = random.randrange(0, 400)
-    y = random.randrange(0, 400)
-    snow_list.append([x, y])
+background = pygame.image.load(os.path.join("assets", "screen_tree.jpg"))
+background = pygame.transform.scale(background, SIZE)
 
 clock = pygame.time.Clock()
 
+# pygame.mouse.set_visible(False)
+# pygame.event.set_grab(True)
+
 done = False
 while not done:
-
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
+
     screen.fill(WHITE)
+    screen.blit(background, (0,0))
+    pygame.display.update()
+
 
 pygame.quit()
 quit()
