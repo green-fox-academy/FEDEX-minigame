53f54611a2be2b096a451c1e5c7ba0204684cc91
tamara-0527 <szigeti.tamara@gmail.com>
2018-01-10 23:04:10 +0100

    feat(basket): add start position of the basket

--- game.py
+++ game.py
@@ -22,6 +22,8 @@ background = pygame.transform.scale(background, SIZE)
 clock = pygame.time.Clock()
 fruits = [pygame.image.load(os.path.join("assets", 'orange.png')), pygame.image.load(os.path.join("assets", 'banana.png')), pygame.image.load(os.path.join("assets", 'melon.png')), pygame.image.load(os.path.join("assets", 'apple.png'))]
 one_fruit = random.choice(fruits)
+basket_left = pygame.image.load(os.path.join("assets", "basket_left.png"))
+basket_right = pygame.image.load(os.path.join("assets", "basket_right.png"))
 
 
 # pygame.mouse.set_visible(False)
@@ -31,19 +33,20 @@ while not done:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
-
     screen.fill(WHITE)
     screen.blit(background, (0,0))
-    
+
     for i in range(100):
         screen.blit(one_fruit, (x, y))
         y += 0.05
         if y > 600:
             y = random.randrange(-50, -10)
             x = random.randrange(0, 600)
+    screen.blit(basket_left, (300,500))
     pygame.display.flip()
     pygame.display.update()
     clock.tick(60)
 
+    
 pygame.quit()
 quit()
